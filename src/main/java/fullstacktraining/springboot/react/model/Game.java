bd9e78bef06f37714293b3ea92962afffb8b702b
brasilindiaconn <brasilindiaconn@gmail.com>
2021-06-02 20:40:48 +0200

    constrains added in the game model for inputs

--- src/main/java/fullstacktraining/springboot/react/model/Game.java
+++ src/main/java/fullstacktraining/springboot/react/model/Game.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -18,9 +19,24 @@ public class Game {
     @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @Column(nullable = false)
+    @NotBlank(message = "PictureUrl is required")
     private String pictureUrl;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Genre is required")
     private String genre;
+
+    @Column(nullable = false)
+    @NotBlank(message = "GameUrl is required")
     private String gameUrl;
 }
